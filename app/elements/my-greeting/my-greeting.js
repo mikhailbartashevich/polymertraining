88ca318b36e755ba7c64cefa7b4ac77ebfba8c5e
Mikhail Bartashevich <mbartashevich@mbartashevich-macbookpro.roam.corp.google.com>
2016-05-03 19:51:59 +0200

    files

---
+++ app/elements/my-greeting/my-greeting.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+  Polymer({
+    is: 'my-greeting',
+
+    properties: {
+      greeting: {
+        type: String,
+        value: 'Welcome!',
+        notify: true
+      }
+    }
+  });
+})();
